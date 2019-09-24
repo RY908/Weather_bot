923cb4ad24bcdbf69a4715eb50b43a8e78fea279
Ryusei <ryuuryuu908@gmail.com>
2019-09-24 10:36:11 +0900

    new commit

--- main.py
+++ main.py
@@ -86,12 +86,12 @@ def handle_message(event):
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location(event):
     text = event.message.address
-
+    """
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=text)
     )
-
+    """
     result = sc.get_weather_from_location(text)
     line_bot_api.reply_message(
         event.reply_token,
