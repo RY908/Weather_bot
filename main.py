cfc4b8f597aa1c9db8f24e355ae864e87e588c2c
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2019-11-04 21:38:41 +0900

    new commit

--- main.py
+++ main.py
@@ -94,11 +94,11 @@ def handle_message(event):
         path = " https://rn-1.herokuapp.com/static/videos/" + user_id + ".mp4"
         new_path = text + ".mp4"
         if not os.path.exists(path):
-            os.rename(path, new_path)  
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=text)
             )
+            os.rename(path, new_path)  
         uploadVideo(new_path)
 
     else:
