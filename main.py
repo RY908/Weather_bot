3c61bfb8e88384d9dc4565a09a1a84cfd2bc56f2
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2020-03-15 15:11:33 +0900

    new commit

--- main.py
+++ main.py
@@ -19,14 +19,7 @@ import urllib3.request
 import os
 import json
 import sys
-import gspread
 import requests
-from oauth2client.service_account import ServiceAccountCredentials
-
-#from spreadsheet import EditSpreadSheet
-
-#from drive import uploadVideo, rename, download
-
 import re
 
  
@@ -61,23 +54,12 @@ def callback():
     # handleの処理を終えればOK
     return 'OK'
  
-## 2 ##
-###############################################
-#LINEのメッセージの取得と返信内容の設定(オウム返し)
-###############################################
- 
-#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
-#def以下の関数を実行します。
-#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
-#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
     user_id = event.source.user_id 
-    #worksheet = EditSpreadSheet()
     if '位置情報' in text:
-        worksheet.add_user_id(user_id)
         line_bot_api.reply_message(
             event.reply_token,
             [
@@ -86,112 +68,17 @@ def handle_message(event):
             ]
         )
 
-    """
-    elif re.match('20\d{6}.+', text):
-        
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=text)
-        )
-        path = " https://rn-1.herokuapp.com/static/videos/" + user_id + ".mp4"
-        new_path = text + ".mp4"
-        uploadVideo(path)
-        if not os.path.exists(path):
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=text)
-            )
-            os.rename(path, new_path)  
-        uploadVideo(new_path)
-        rename(user_id, text)
-        message = "送信が完了しました。"
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=message)
-        )
-
-    elif 'ダウンロード' in text:
-        link = download("20191111あ")
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=link)
-        )
-
-    else:
-        
-        path = os.path.dirname(os.path.abspath(sys.argv[0]))
-        path = path + '/info.json'
-        with open(path) as f:
-            d_update = json.load(f)
-            #result = (json.load(f))
-        d_update[user_id] = 'a'
-        result = d_update[user_id]
-        with open (path, 'w') as f:
-            json.dump(d_update, f)
-        result = "test"
-        scope = ['https://spreadsheets.google.com/feeds',
-                'https://www.googleapis.com/auth/drive']
-
-        path = os.path.dirname(os.path.abspath(sys.argv[0]))
-        path += '/rn-1-a615ac4d9dff.json'
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
-        gc = gspread.authorize(credentials)
-        wks = gc.open('info').sheet1
-
-        wks.update_acell('A1', 'testtest')
-        #print(wks.acell('A1'))
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=result)
-        )"""
   
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location(event):
-    worksheet = EditSpreadSheet()
     user_id = event.source.user_id
     text = event.message.address
 
     result, location = sc.get_weather_from_location(text)
-    worksheet.add_user_location(user_id, location)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=result)
     )
-"""
-@handler.add(MessageEvent, message=VideoMessage)
-def handle_video(event):
-    message_id = event.message.id
-    user_id = event.source.user_id 
-    message_content = line_bot_api.get_message_content(message_id)
-    
-    url = "https://api-data.line.me/v2/bot/message/{}/content".format(message_id)
-    print(url)
-    res = requests.get(url, stream=True, headers={"Authorization": "Bearer {}".format(YOUR_CHANNEL_ACCESS_TOKEN)})
-    path = user_id + ".mp4"
-    print(res.status_code)
-    #if res.status_code == 200:
-
-    with open(path, 'wb') as file:
-        for chunk in res.iter_content(chunk_size=1024):
-            file.write(chunk)
-        
-    
-    with open(path, 'wb') as file:
-        for chunk in res.iter_content():
-            file.write(chunk)
-    
-    #path = "static/videos/" + user_id + ".mp4"
-    path = user_id + ".mp4"
-    with open(path, 'wb') as fd:
-        for chunk in message_content.iter_content():
-            fd.write(chunk)
-    
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='ファイル名を送信してください。')
-    )
-    uploadVideo(path)"""
 
 
 # ポート番号の設定
